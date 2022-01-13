fb660b43958efdd466c958f5a9751049c8177a42
harrybark <harrypmw@gmail.com>
2022-01-14 00:12:58 +0900

    h2 database test

--- src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -2,19 +2,29 @@ package com.fastcampus.jpa.bookmanager.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 @Builder
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 
     @NonNull
     private String name;
+
     @NonNull
     private String email;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
